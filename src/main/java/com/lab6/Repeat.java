1585d8d2e7f5d7de47ae6ecd8623152830cb85cc
LoliIRL <maximkem2004@gmail.com>
2025-10-25 17:43:38 +0700

    Создана пользовательская аннотация @Repeat

---
+++ src/main/java/com/lab6/Repeat.java
@@ -0,0 +1,16 @@
+﻿package com.lab6;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Аннотация для повторного выполнения методов
+ * times - количество повторений выполнения метода
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Repeat {
+    int times() default 1;
+}
